8bad21b690e50eaa2ae171bc1833424f5f10496d
Valerie <valeriejeptoo4@gmail.com>
2023-06-18 15:01:19 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Valerie Kandagor';
+
+const welcomeMessage = `Welcome to Flatbook, ${'Valerie Kandagor'}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${'Valerie Kandagor'.slice(0, 1)}!`;
